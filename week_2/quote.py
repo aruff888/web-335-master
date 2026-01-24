1ee9e8fe4383ae9eecd7d4f2ee9809a9f4f4432a
Amanda <aruff@my365.bellevue.edu>
2026-01-23 21:05:16 -0600

    add week_2/quote.py

---
+++ week_2/quote.py
@@ -0,0 +1,10 @@
+"""
+Author: Amanda Ruff
+Date: January 23, 2026
+File Name: quote.py
+Description: This program prints a favorite quote and credits the original author.
+"""
+
+# Printing a quote and its author
+print("“The only way to do great work is to love what you do.”")
+print("- Steve Jobs")
